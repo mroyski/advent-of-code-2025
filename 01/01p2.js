d55a5060643cc28bcf6194f1754fb40a53081a62
Michael Royski <mroyski@gmail.com>
2025-12-02 23:07:29 -0500

    day 1

--- 01/01p2.js
+++ 01/01p2.js
@@ -21,33 +21,47 @@ for (let i = 0; i < input.length; i++) {
   let move = parseInt(input[i].slice(1));
   let direction = input[i][0];
 
+  console.log('======= Processing move:', input[i]);
+
   while (move > 0) {
     console.log('direction:', direction, 'location:', location, 'move:', move);
     if (direction === 'L') {
+      if (location === 0) {
+        location = 100;
+      }
+
       if (location - move < 0) {
         move = move - location;
         location = 100;
+        result++;
       } else {
         location = location - move;
         move = 0;
+        if (location === 0) {
+          result++;
+        }
       }
     } else {
-        if (location + move > 100) {
-            move = move - (100 - location);
-            location = 0;
-        } else {
-            location = location + move;
-            move = 0;
+      if (location === 100) {
+        location = 0;
+      }
+
+      if (location + move >= 100) {
+        move = move - (100 - location);
+        location = 0;
+        result++;
+      } else {
+        location = location + move;
+        move = 0;
+        if (location === 100) {
+          result++;
         }
+      }
     }
   }
 
-  console.log('location after move:', location);
-
-  if (location === 0 || location === 100) {
-    result++;
-  }
+  console.log('location after move:', location, 'result:', result);
 }
 
-// 1031
+// 5831
 console.log('result:', result);
